da507b4d52805cf6a5d2446f3e0025a9a9a97173
LZERDOUN <laura.gaguech@gmail.com>
2023-01-17 22:03:52 +0200

    Gestion erreurs CORS

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require("express");
 
 const app = express();
 
+//Gestion erreurs de CORS (origines différentes)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use((req, res, next) => {
   console.log("Requête reçue !");
   next();
