b7eca4dbd5851312bed65d0d937d6a848dddc91e
LZERDOUN <laura.gaguech@gmail.com>
2023-01-19 19:57:09 +0200

    Ajout d'un User Schema

--- models/user.js
+++ models/user.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
+//Package facilitant la tâche de vérification d'un email unique
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+//Appliquer le validateur sur notre schéma
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", userSchema);
